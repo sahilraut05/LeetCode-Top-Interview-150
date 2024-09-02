a2f64b68aab10e9a85f537040523f5cb2ecaff27
Sahil Raut <raut.sahil003@gmail.com>
2024-09-02 17:19:06 +0530

    Commit

--- 006.LeetCode@189_Rotate_Array/006.LeetCode@189_Rotate_Array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        length = len(nums)
-        k = k % length
-
-        temp = nums[-k:]
-        nums[k:] = nums[:-k]
-        nums[:k] = temp
