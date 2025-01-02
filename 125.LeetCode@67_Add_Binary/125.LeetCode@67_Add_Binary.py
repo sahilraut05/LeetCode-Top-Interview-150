6d940118b876599deddfe12bc9844abdff6159f8
Sahil Raut <raut.sahil003@gmail.com>
2025-01-02 11:56:15 +0530

    Commit

---
+++ 125.LeetCode@67_Add_Binary/125.LeetCode@67_Add_Binary.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = []
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+
+        while i >= 0 or j >= 0:
+            sum = carry
+            if i >= 0:
+                sum += int(a[i])
+                i -= 1
+            if j >= 0:
+                sum += int(b[j])
+                j -= 1
+            carry = 1 if sum > 1 else 0
+            res.append(str(sum % 2))
+
+        if carry:
+            res.append('1')
+
+        return ''.join(res[::-1])
